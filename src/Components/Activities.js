41ee3a1a3aba68c47a345ac8457bcbf3320fcd6d
Angelica1970 <angelbaldwin013@gmail.com>
2023-02-15 20:44:52 -0600

    activity

--- src/Components/Activities.js
+++ src/Components/Activities.js
@@ -6,16 +6,19 @@ const Activities =(props) =>{
 
     return <ul>{
                    activities.map((activity)=>{
-                     return <li>
-                        <div>
+                     return <li key={activity.id} className="activity">
+                        {<div>
+                             
+                             <div className="ByLine"><h4>{activity.name}</h4><h5>By: {activity.creatorName}</h5></div>
                             <p><b>Name: </b>{activity.name}</p>
                             <p><b>Description: </b>{activity.description}</p>
-                        </div>
-                     </li>
-                   })
-
-
-        }</ul>
+                      
+                          </div>
+                        }
+                      </li>
+                   })                 
+             }
+             </ul>
 
 
 }
