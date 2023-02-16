e579378f502e51354c076d6e8dd4b5368b6a0d81
bryan menegussi <bryanmenegussi@gmail.com>
2023-02-16 11:57:17 -0600

    trying to display myRoutines

--- src/Components/MyRoutines.js
+++ src/Components/MyRoutines.js
@@ -1,16 +1,23 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
+const MyRoutines = (props) => {
+  const { user } = props.user;
+  const myRoutines = props.routines;
+  const myActivities = props.activities;
+  console.log(props);
+  const [userRoutines, setUserRoutines] = useState([]);
 
-const MyRoutines = (props) =>{
-    const {user} = props;
-    console.log(user);
-    const [userRoutines, setUserRoutines] = useState([]);
-
-    return<div>
-        <h2>${user.username}</h2>
+  if(!user.username){
+    return <h1>log in to view your routines</h1>
+  }
+    else { 
+        return (
+            <form>
+    <div>
+      <h2>${user.username}</h2>
     </div>
+    </form>
+  );
+};
 }
-
-  
-
 export default MyRoutines
