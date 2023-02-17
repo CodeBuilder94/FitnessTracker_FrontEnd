7072b5f354268b5d852f9ee45614c9f7b3e4c859
CodeBuilder94 <tyler.c.mitchell@verizon.net>
2023-02-16 22:10:10 -0600

    Got the My Routines to show up and dispay. Also fixed other problems regarding the user being logged in. Submitting a routine is almost done.

--- src/Components/MyRoutines.js
+++ src/Components/MyRoutines.js
@@ -1,13 +1,58 @@
 import React, {useState} from "react";
-
+import {getUserRoutines, stayIn} from "../api";
+import { CreateRoutine } from "/";
 
 const MyRoutines = (props) =>{
-    const {user} = props;
-    console.log(user);
+    const {user, setUser, token} = props;
+    
     const [userRoutines, setUserRoutines] = useState([]);
 
+    
+    const getRoutines = async() =>{
+        
+
+        const routineList = await getUserRoutines(user);
+        setUserRoutines(routineList);
+    }
+
+    const getToken = async() =>{
+        const token = window.localStorage.getItem("token");
+        const userInfo =await stayIn(token);
+        setUser(userInfo);
+        console.log(user);
+    }
+
+    if(userRoutines)
+    {
+        if(!user)
+        {
+            getToken();
+        }
+          
+        getRoutines(); 
+    }
+    
+
     return<div>
-        <h2>${user.username}</h2>
+        {user ?<div>
+        <h2>welcome: {user.username}</h2>
+        <ul>
+        {
+            <div>
+                {userRoutines ?
+            userRoutines.map((routine) =>
+            {
+                return <li key={routine.id}>
+                    <h3>{routine.name}</h3>
+                    <p>Goal: {routine.goal}</p>
+                </li>
+            })
+            :null }
+            </div>
+        }
+        </ul>
+        </div>:null}
+        <CreateRoutine/>
     </div>
 }
 
